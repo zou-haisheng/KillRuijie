74587043563ff2efe6b9300f7103f356b3b84020
zou-haisheng <58100864+zou-haisheng@users.noreply.github.com>
2023-03-19 15:31:57 +0800

    Create

---
+++ RESUME.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stdio.h>
+#include<windows.h>
+using namespace std;
+
+namespace resume
+{
+	class ruijie{
+		public:
+			char kill_name[100]="RG-CloudManagerRemote.exe";
+			string del[10]={"C:\\Program Files (x86)\\RG-CloudManagerRemote\\install.ini",
+				        "C:\\Program Files (x86)\\RG-CloudManagerRemote\\Launcher.ini",
+			            "C:\\Program Files (x86)\\RG-CloudManagerRemote\\version.ini"};
+			string resume[10]={"install.ini",
+						   "Laucher.ini",
+						   "version.ini"};
+			int __del(string s)
+			{
+				DeleteFile(s.c_str());
+			}
+			void kill_process(char task_name[])
+			{
+				system("taskkill /f /im CMLauncher.exe /t");
+				for(int i=0;i<3;i++) __del(del[i].c_str());
+			}
+			void __resume()
+			{
+				for(int i=0;i<3;i++) system(("copy "+resume[i]+" C:\\Program Files (x86)\\RG-CloudManagerRemote").c_str());
+			}
+	};
+
+	int main()
+	{
+		
+		ruijie rj;
+		rj.__resume();
+		return 0;
+	}
+}
+
+int main()
+{
+	resume::main();
+	return 0;
+}
